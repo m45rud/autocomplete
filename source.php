74a4d2d7af6e1589e6c9a0c62fb6cd36638f3365
m45rud <m45rud@gmail.com>
2018-12-24 19:24:44 +0700

    Update

--- source.php
+++ source.php
@@ -1,44 +1,33 @@
 <?php
+// Set header type konten.
+header("Content-Type: application/json; charset=UTF-8");
 
-    // Cegak akses langsung ke source Ajax.
-    if ( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && ( $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest' ) ) {
+// Deklarasi variable untuk koneksi ke database.
+$host     = "localhost";// Server database
+$username = "root";     // Username database
+$password = "root";     // Password database
+$database = "autocomplete";     // Nama database
 
-        // Set header type konten.
-        header("Content-Type: application/json; charset=UTF-8");
+// Koneksi ke database.
+$conn = new mysqli($host, $username, $password, $database);
 
-        // Deklarasi variable untuk koneksi ke database.
-        $host     = "localhost";        // Server database
-        $username = "root";             // Username database
-        $password = "masrud.com";       // Password database
-        $database = "lab";     			// Nama database
+// Deklarasi variable keyword buah.
+$buah = $_GET["query"];
 
-        // Koneksi ke database.
-        $conn = new mysqli($host, $username, $password, $database);
+// Query ke database.
+$query  = $conn->query("SELECT * FROM table_buah WHERE buah LIKE '%$buah%' ORDER BY buah DESC");
+$result = $query->fetch_all(MYSQLI_ASSOC);
 
-        // Deklarasi variable keyword buah.
-        $buah = $_GET["query"];
+// Format bentuk data untuk autocomplete.
+foreach($result as $data)
+{
+    $output['suggestions'][] = [
+'value' => $data['buah'],
+'buah'  => $data['buah']
+    ];
+}
 
-        // Query ke database.
-        $query = $conn->query("SELECT * FROM buah WHERE buah LIKE '%$buah%' ORDER BY buah DESC");
-        $result = $query->fetch_all(MYSQLI_ASSOC);
-
-        // Format bentuk data untuk autocomplete.
-        foreach($result as $data)
-        {
-            $output['suggestions'][] = [
-                'value' => $data['buah'],
-                'buah'  => $data['buah']
-            ];
-        }
-
-        if (!empty($output)) {
-            // Encode ke format JSON.
-            echo json_encode($output);
-        }
-
-    } else {
-
-        // Tampilkan peringatan.
-        echo 'No direct access source!';
-    }
-?>
+if (! empty($output)) {
+    // Encode ke format JSON.
+    echo json_encode($output);
+}
